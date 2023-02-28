cce14461f875b71b32d1369983df60fab7b4ab5d
juhee <jh7895214@gmail.com>
2023-02-28 22:36:31 +0900

    jdbc-1 commit

---
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,10 @@
+package hello.jdbc.connection;
+
+// abstract 객체 생성 막음
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+    
+
+}
